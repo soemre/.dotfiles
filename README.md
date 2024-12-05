37c16bbd2f8b748c8d97db9b519294489122fea8
soemre <emresoysuren03@gmail.com>
2024-12-05 10:12:50 +0100

    welcome to the future

--- README.md
+++
@@ -1,6 +0,0 @@
-## About
-This repository includes my configuration files.
-
-## Included Configs:
-- Git
-- VSCode
---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+remove_flag="";
+
+dots_dir="$(dirname "$(realpath "$0")")";
+
+cfg_dir="$HOME/.config"
+
+# Link the dotfiles
+declare -A dst=(
+    [".gitconfig"]="$HOME"
+    [".tmux.conf"]="$HOME"
+    ["nvim"]="$cfg_dir"
+    ["alacritty"]="$cfg_dir"
+)
+
+
+# Interpret the flags
+while getopts "r" flag; do
+    case "${flag}" in
+        r) remove_flag=true;;
+        *);;
+    esac
+done
+
+for src in "${!dst[@]}"
+do
+    to="${dst[$src]}/$src"
+
+    if [[ $remove_flag ]]; then
+        echo "Removing the link form $to"
+        rm "$to"
+    else
+        from="$dots_dir/$src"
+        echo "Linking $from to $to"
+        ln -s "$from" "$to"
+    fi
+done
+
