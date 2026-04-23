943fb748c0c9b1cfdf8a67d232a253a57cea86c2
soemre <emresoysuren03@gmail.com>
2026-04-23 11:40:33 +0200

    include nvim changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
+    atuin init fish --disable-up-arrow | source
+    thefuck --alias | source
 end
 
 set fish_greeting
 zoxide init fish | source
+
+fish_add_path -aP $HOME/.pub-cache/bin
